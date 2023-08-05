65ff6db5bc0d163003f8e43353ad06fc21c4ad06
pidokige02 <pidokige0204@gmail.com>
2023-08-05 11:24:47 +0900

    Started change email view

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 from rest_framework.views import APIView
@@ -11,3 +11,9 @@ class FileUploadView(APIView):
     def post(self, request, *args, **kwargs):
         return Response({"test": True}, status=HTTP_200_OK)
 
+
+class ChangeEmailView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def post(self, request, *args, **kwargs):
+        return Response({"test": True}, status=HTTP_200_OK)
